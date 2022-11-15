7d1c219727c1be5952f47e86d20f5a50a755f42b
jorijnsmit <mail@gosuto.ai>
2022-11-15 12:41:52 +0200

    feat: pick random name from list, add user class `__repr__`

--- scripts/cdp_sim.py
+++ scripts/cdp_sim.py
@@ -29,147 +29,159 @@
 
   Liquidate
 """
+import random
+
+from lib.names import name_list
+
 
 MAX_BPS = 10_000
-
-
 
 GLOBAL_TOTAL_DEPOSITS = 0
 GLOBAL_TOTAL_DEBT = 0
 
 INTERNAL_SECONDS_SINCE_DEPLOY = 0
 
-MAX_LTV = 15000 ## 150%
-FEE_PER_SECOND = 0 ## No fee for borrows
-ORIGINATION_FEE = 50 ## 50BPS
+MAX_LTV = 15000  ## 150%
+FEE_PER_SECOND = 0  ## No fee for borrows
+ORIGINATION_FEE = 50  ## 50BPS
 
 FEED = 1000
 
+
 def GLOBAL_COLLATERAL_RATIO():
-  return GLOBAL_TOTAL_DEBT * MAX_BPS / GLOBAL_TOTAL_DEPOSITS 
+    return GLOBAL_TOTAL_DEBT * MAX_BPS / GLOBAL_TOTAL_DEPOSITS
+
 
 def GLOBAL_MAX_BORROW():
-  GLOBAL_TOTAL_DEBT * FEED
+    GLOBAL_TOTAL_DEBT * FEED
+
 
 def IS_IN_EMERGENCY_MODE():
-  ## TODO:
-  return False
+    ## TODO:
+    return False
+
 
 def IS_SOLVENT():
-  ## NOTE: Strictly less to avoid rounding, etc..
-  return GLOBAL_TOTAL_DEBT < GLOBAL_MAX_BORROW()
+    ## NOTE: Strictly less to avoid rounding, etc..
+    return GLOBAL_TOTAL_DEBT < GLOBAL_MAX_BORROW()
 
 
-class Trove():
-  def __init__(self, owner):
-    self.deposits = 0
-    self.debt = 0
-    self.last_update_ts = INTERNAL_SECONDS_SINCE_DEPLOY
-    self.owner = owner
-  
-  def local_collateral_ratio(self):
-    return self.debt * MAX_BPS / self.deposits
-  
-  def deposit(self, amount):
-    GLOBAL_TOTAL_DEPOSITS += amount
-    self.deposits += amount
+class Trove:
+    def __init__(self, owner):
+        self.deposits = 0
+        self.debt = 0
+        self.last_update_ts = INTERNAL_SECONDS_SINCE_DEPLOY
+        self.owner = owner
 
-    self.owner.reduce_balance(self, amount)
-  
-  def withdraw(self, amount):
-    self.deposits -= amount
+    def local_collateral_ratio(self):
+        return self.debt * MAX_BPS / self.deposits
 
-    assert self.is_solvent()
-  
-  def borrow(self, amount):
-    self.debt += amount
+    def deposit(self, amount):
+        global GLOBAL_TOTAL_DEPOSITS
+        GLOBAL_TOTAL_DEPOSITS += amount
+        self.deposits += amount
 
-    assert self.is_solvent()
-  
-  def repay(self, amount):
-    ## TODO
-    return 0
-  
-  def liquidate(self, amount, caller):
-    ## Only if not owner
-    if caller == self.owner:
-      return False
-    
-    return 0
+        self.owner.reduce_balance(self, amount)
 
-  
-  ## SECURITY CHECKS
-  def is_trove():
-    return True
+    def withdraw(self, amount):
+        self.deposits -= amount
 
-  def max_borrow(self):
-    return self.deposits * FEED
-  
-  def is_solvent(self):
-    ## Strictly less to avoid rounding or w/e
-    return self.debt < self.max_borrow() 
-  
+        assert self.is_solvent()
 
-  
+    def borrow(self, amount):
+        self.debt += amount
 
-class User():
-  def __init__(self, initial_balance_collateral, name):
-    self.collateral = initial_balance_collateral
-    self.name = name ## TODO: Random Name Generator
-  
-  def increase_balance(self, caller, amount):
-    try:
-      assert caller.is_trove() == True
-      self.collateral += amount
+        assert self.is_solvent()
 
-    except:
-        ## Do nothing on failure
-        print("error")
-    
-  def reduce_balance(self, caller, amount):
-    try:
-      assert caller.is_trove() == True
-      self.collateral -= amount
+    def repay(self, amount):
+        ## TODO
+        return 0
 
-    except:
-        ## Do nothing on failure
-        print("error")
-  
-  def get_balance(self):
-    return self.collateral
-  
+    def liquidate(self, amount, caller):
+        ## Only if not owner
+        if caller == self.owner:
+            return False
+
+        return 0
+
+    ## SECURITY CHECKS
+    def is_trove(self):
+        return True
+
+    def max_borrow(self):
+        return self.deposits * FEED
+
+    def is_solvent(self):
+        ## Strictly less to avoid rounding or w/e
+        return self.debt < self.max_borrow()
+
+
+class User:
+    def __init__(self, initial_balance_collateral):
+        self.collateral = initial_balance_collateral
+        self.name = random.choice(name_list)
+
+    def __repr__(self):
+        return str(self.__dict__)
+
+    def increase_balance(self, caller, amount):
+        try:
+            assert caller.is_trove() == True
+            self.collateral += amount
+
+        except:
+            ## Do nothing on failure
+            print("error")
+
+    def reduce_balance(self, caller, amount):
+        try:
+            assert caller.is_trove() == True
+            self.collateral -= amount
+        except:
+            ## Do nothing on failure
+            print("error")
+
+    def get_balance(self):
+        return self.collateral
 
 
 ## TODO: Add Roles ##
 
+## Base Class
+class Actor:
+    pass
+
+
 ## Borrows and Holds
 class Borrower(Actor):
-
     def step():
         pass
+
 
 ## Borrow and Sells when price is higher
 class LongArbitrager(Actor):
-
     def step():
         pass
+
 
 ## Buys when cheap and sells when higher
 class ShortArbitrager(Actor):
-
     def step():
         pass
 
+
 ## Does both arbitrages
 class Trader(Actor):
-
     def step():
         pass
 
 
 def main():
-  user_1 = User(100, "A")
+    user_1 = User(100)
 
-  ## TODO: Proper Print
-  print(user_1)
+    print(user_1)
 
+    trove_1 = Trove(user_1)
+    trove_1.deposit(25)
+
+    print(user_1)
