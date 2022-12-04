84a9cc63fd754e11ffd6cb306e9634a87d51f079
Entreprenerd <alex@entreprenerd.xyz>
2022-12-04 18:40:23 +0100

    feat: price impact math

--- scripts/amm_price_impact_sim.py
+++ scripts/amm_price_impact_sim.py
@@ -34,6 +34,8 @@ BTC_DECIMALS = 8
 MAX_LTV = 8_500 ## 85% LTV
 MAX_BPS = 10_000
 
+MAX_LP_PERCENT = MAX_BPS
+
 ## At risk BTC -> Needs to be liquidated
 ## Available in AMM BTC -> Can be bought at price impact before thinking about taking debt
 """
@@ -49,19 +51,31 @@ MAX_BPS = 10_000
   Other MM would not want to engage with that, and would rather liquidate and make a arb ETH -> ETH
 """
 
-def main():
+def sim():
   AVG_LTV = random() * MAX_LTV
-  MAX_RISK_PERCENT = random() * MAX_LTV
-  LP_PERCENT = random() * MAX_BPS
+  MAX_RISK_PERCENT = random() * MAX_BPS
+  LP_PERCENT = random() * MAX_LP_PERCENT
+  
+  ## Always greater than MAX_LTV
+  ## But smaller than 100%
+  ## At 100% + 1 we have no economic incentive to save
+  AT_RISK_LTV = MAX_LTV + random() * (MAX_BPS - MAX_LTV)
 
   deposited_eth = AMT_ETH * 10 ** ETH_DECIMALS
   print("deposited_eth", deposited_eth)
   borrowed_btc = deposited_eth / (10 ** ETH_DECIMALS - BTC_DECIMALS) * AVG_LTV / MAX_BPS
   print("borrowed_btc", borrowed_btc)
 
-  max_liquidatable = borrowed_btc * MAX_RISK_PERCENT
+  max_liquidatable = borrowed_btc * MAX_RISK_PERCENT / MAX_BPS
+
+  print("max_liquidatable", max_liquidatable)
+  print("as percent", MAX_RISK_PERCENT)
+
+  ## TODO: Check if correct or if it's just floating math
+  # assert MAX_RISK_PERCENT == max_liquidatable * MAX_BPS / borrowed_btc
 
   ## Assume 13 ETH = 1 BTC to KIS
+  ## NOTE: Assume LP value is 1/1 which technically is naive, but for sim purposes should be sufficient
   price_ratio = 13
 
   in_amm = LP_PERCENT * borrowed_btc / MAX_BPS
@@ -70,16 +84,82 @@ def main():
   reserve_btc = in_amm
   reserve_eth = as_eth
 
-  print("price_given_in(1, reserve_btc, reserve_eth", price_given_in(1, reserve_btc, reserve_eth))
-  initial_price = price_given_in(1, reserve_btc, reserve_eth)
+  print("price_given_in(1, reserve_btc, reserve_eth", price_given_in(1, reserve_eth, reserve_btc))
+  initial_price = price_given_in(1, reserve_eth, reserve_btc)
+  
   ## Price makes sense
-  assert 1 / initial_price > 12 and 1 / initial_price < 13
+  # assert initial_price >= 12 and initial_price <= 14
 
 
   ## What is the maximum I'm willing to pay to get eBTC to liquidate to ETH, if debt is N and I get M out?
 
+  """
+    TODO: Simulate % insolvent
+    Figure out % Profit
+    Given that, figure out max price impact (Which is effectively the same as a reduction in profit as a %)
+    Given that, figure out max purchaseable given the pool
+    Given that buy max and liquidate max
+    Then output those info
+  """
 
+  ## max_liquidatable is the percent we want to liquidate
+  liquidatable_debt = max_liquidatable
+  print("liquidatable_debt", liquidatable_debt)
+  ## TODO: Check MATH
+  liquidatable_collataeral = liquidatable_debt / price_ratio / AT_RISK_LTV * MAX_BPS
+  print("liquidatable_collataeral", liquidatable_collataeral)
+
+  ## Check if we can liquidate all before it being unprofitable
+
+  ## MAX_BPS - AT_RISK_LTV is the BPS Profitability
+  profitability_bps = MAX_BPS - AT_RISK_LTV
+  print("profitability_bps", profitability_bps)
+
+  ## Check current AMM Price * the delta above
+  current_price = price_ratio
+
+  ## Assume we want at least 1 BPS for profitability
+  ## TODO: When adding fees, prob add here a opportunity cost / MEV cost of business
+  max_price = current_price * (MAX_BPS + profitability_bps - 1) / MAX_BPS
+  print("max_price", max_price)
+
+  ## Check amount we can buy
+  max_amount = max_in_before_price_limit(max_price, reserve_eth, reserve_btc)
+  print("max_amount", max_amount)
+
+  ## If we can buy all, then we good
+  if(max_amount >liquidatable_debt):
+    print("We can safely liquidate")
+    return True
+  else:
+    ## Else liquidate only up to what's profitable
+    print("We are capped")
+    return False
   
 
+
+  
+RUNS = 10_000
+
+def main():
+  counter = 0
+  exc = 0
+  insolvent = 0
+  for i in range(RUNS):
+    try:
+      if sim():
+        counter += 1
+      else:
+        insolvent += 1
+    except:
+      print("Something went wrong")
+      exc += 1
+  
+  print("Can safely liquidate", counter, "out of ", RUNS)
+  print("Exceptions (not necessarily insolvent)", exc)
+
+
 if __name__ == '__main__':
-    main()
+  main()
+  
+
