b33a9dcc814074d8ad34e4a1792f52b5d8848fdd
Entreprenerd <alex@entreprenerd.xyz>
2023-03-29 16:42:04 +0200

    chore: initial setup of new script

---
+++ scripts/amm_leverage_math.py
@@ -0,0 +1,9 @@
+## TODO:
+"""
+  Given the code in AMM
+
+  Given X amount in the pool
+  Figure out how much you can loop before you get Y% of slippage
+  Meaning, how many times can you buy Z tokens before the price goes up by Y%
+
+"""
